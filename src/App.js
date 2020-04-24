173277060d908fc63b5c62cfbd9f05678dbdd00a
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-04-24 23:00:39 +0530

    Home page updated

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
             <MDBNavbar color='indigo' dark expand='md' fixed='top' scrolling>
               <MDBNavbarBrand href='/' className='py-0 font-weight-bold'>
                 {/*<Logo style={{ height: '2.5rem', width: '2.5rem' }} />*/}
-                <strong className='align-middle'>Monitoring App</strong>
+                <strong className='align-middle'>Fire Tracker</strong>
               </MDBNavbarBrand>
               <MDBNavbarToggler
                   onClick={this.toggleCollapse('mainNavbarCollapse')}
